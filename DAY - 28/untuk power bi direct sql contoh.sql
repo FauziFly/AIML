b2b49ba27927505c566a30599ba0d79c98aac93d
FauziFly <fauzi.zulkefly@gmail.com>
2025-11-13 16:31:51 +0800

    'updated'

---
+++ DAY - 28/untuk power bi direct sql contoh.sql
@@ -0,0 +1,8 @@
+use OurDB
+select * from Category order by CatId
+select * from Product
+use SalesDB
+select * from Sales
+select * from Products
+
+insert into Sales values (1,'North',4,'2025-11-13')
